02dab34948c75e00847889f2ae333b08a3c000f0
Justin <justin.warambourg@gmail.com>
2024-03-08 16:40:28 +0800

    add helper script to build and deploy docker image

---
+++ utils/docker_utils.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -eox pipefail
+IFS=$'\n\t'
+
+IMG_NAME=prefect-flows
+TAG=latest
+REPO="211125707335.dkr.ecr.ap-northeast-1.amazonaws.com"
+
+echo "Building img=$IMG_NAME:$TAG" 
+if [[ -n $1 ]] && [[ $1 != x ]]; then
+
+	docker build -t $IMG_NAME:$TAG .
+fi 
+
+if [[ -n $2 ]]; then
+	echo "Pushing to repo $REPO/$IMG_NAME as $TAG" 
+
+	docker tag $IMG_NAME:$TAG $REPO/$IMG_NAME:$TAG
+	# docker run -e FLOW_NAME=youbike -it $IMG_NAME:$TAG 
+	docker push $REPO/$IMG_NAME:$TAG
+fi 
