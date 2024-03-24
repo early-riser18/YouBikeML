30256ec8cc783348737a06fd70e3d6c54621859c
Justin <justin.warambourg@gmail.com>
2024-03-24 13:48:22 +0800

    enable auto login before pushing to ecr

--- utils/docker_utils.sh
+++ utils/docker_utils.sh
@@ -14,7 +14,7 @@ fi
 
 if [[ -n $2 ]]; then
 	echo "Pushing to repo $REPO/$IMG_NAME as $TAG" 
-
+	aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 211125707335.dkr.ecr.ap-northeast-1.amazonaws.com
 	docker tag $IMG_NAME:$TAG $REPO/$IMG_NAME:$TAG
 	# docker run -e FLOW_NAME=youbike -it $IMG_NAME:$TAG 
 	docker push $REPO/$IMG_NAME:$TAG
