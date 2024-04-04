a7207cc1d1cc409e440a289fa98b3ef300ad5200
Justin <justin.warambourg@gmail.com>
2024-04-04 14:06:01 +0800

    fix: remove invalid records from prediction features

--- transform/features_creation.py
+++ transform/features_creation.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from abc import ABC, abstractmethod, abstractproperty
+from abc import ABC, abstractmethod
 from utils.s3_helper import ConnectionToS3, download_from_bucket
 from extraction.youbike import extract_youbike_raw_data
 from transform.clean_youbike_data import clean_youbike_data
@@ -17,7 +17,7 @@ class FeaturesCreator(ABC):
     ) -> pd.DataFrame:
         pass
 
-    @abstractproperty
+    @property
     def model_name_version(self) -> str:
         pass
 
@@ -44,6 +44,10 @@ class FeaturesCreator_v1(FeaturesCreator):
         # Remove records for features creation
         main_df = main_df[main_df["extraction_ts"] == main_df["extraction_ts"].max()]
 
+        #>>>>>>>> 
+        #TEMP HOTFIX - See https://www.notion.so/justinwarambourg/Review-conditions-for-valid-Youbike-record-7173f198f387442da15185201da6551f?pvs=4
+        main_df = main_df[~main_df["pct_full"].isna()].copy(deep=True)
+        #<<<<<<<<
         # Format and Validate to Schema
         main_df = features_lib.FormatToFeaturesSchema("pandas").run(main_df)
         is_schema_valid = features_lib.ValidateFeaturesSchema("pandas").run(main_df)
@@ -60,32 +64,9 @@ class FeaturesCreator_v1(FeaturesCreator):
 
 
 if __name__ == "__main__":
-    test_station_ids = [
-        508201032,
-        508201033,
-        508201034,
-        508201035,
-        508201036,
-        508201037,
-        508201038,
-        508201039,
-        508201040,
-        508201041,
-        508201013,
-        508201014,
-        508201015,
-        508201016,
-        508201017,
-        508201018,
-        508201019,
-        508201020,
-        508201021,
-        508201022,
-        508201023,
-        508201024,
-        508201025,
-        508201026,
-    ]
+    
+    test_station_ids = pd.read_csv("./tmp/all_stations_id.csv")['id'].tolist()
+
     print(
         FeaturesCreator_v1()
         .make_prediction_features(test_station_ids)
