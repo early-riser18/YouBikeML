8dee8bfed17a3ac86680ca2693cb1d6dc9ca0dba
Justin <justin.warambourg@gmail.com>
2024-04-17 10:24:59 +0800

    hotfix: remove stations with no weather zone

--- transform/features_creation.py
+++ transform/features_creation.py
@@ -51,7 +51,6 @@ class FeaturesCreator_v1(FeaturesCreator):
         #TEMP HOTFIX see https://www.notion.so/justinwarambourg/Setup-new-Bike-Station-flow-4e592b533f9c48afa359c8b21fcc5228?pvs=4
         main_df = main_df[~main_df["wind_speed"].isna()].copy(deep=True)
         #TEMP HOTFIX - See https://www.notion.so/justinwarambourg/Review-conditions-for-valid-Youbike-record-7173f198f387442da15185201da6551f?pvs=4
-
         main_df = main_df[~main_df["pct_full"].isna()].copy(deep=True)
         #<<<<<<<<
         # Format and Validate to Schema
