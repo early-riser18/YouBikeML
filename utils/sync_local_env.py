a32e670f923b6994412094a2e8aee84a60ba3f2b
Justin <justin.warambourg@gmail.com>
2024-05-06 10:25:11 +0800

    add skip download when file already downloaded

--- utils/sync_local_env.py
+++ utils/sync_local_env.py
@@ -16,16 +16,21 @@ def sync_local_from_stage(filter: str, limit: int = 9999):
         reverse=True,
     )
 
+    key_in_storage = local_co.Bucket.objects.filter(Prefix=filter)
     # download and upload
     for key in key_to_dl[:limit]:
-        res = stage_co.Bucket.Object(key).get()
-        export_file_to_s3(local_co, key, res["Body"].read())
-        print(f"uploaded {key} at {local_co.bucket_name}")
+        if key not in [obj.key for obj in key_in_storage]:
+            res = stage_co.Bucket.Object(key).get()
+            export_file_to_s3(local_co, key, res["Body"].read())
+            print(f"uploaded {key} at {local_co.bucket_name}")
+        else:
+            print(f"{key} already downloaded. Skipping it.")
 
 
 if __name__ == "__main__":
-    sync_local_from_stage("raw_data/youbike_dock_info_202", limit=20)
-    sync_local_from_stage("clean_data/youbike_dock_info_202", limit=20)
+    sync_local_from_stage("raw_data/youbike_dock_info_202", limit=200)
+    sync_local_from_stage("clean_data/youbike_dock_info_202", limit=200)
     sync_local_from_stage("raw_data/weather/forecast_report", limit=1)
-    sync_local_from_stage("raw_data/weather/forecast_report", limit=0)
+    sync_local_from_stage("raw_data/weather/forecast_report", limit=1)
     sync_local_from_stage("model", limit=1)
+    
