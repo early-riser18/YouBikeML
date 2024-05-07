f7e137235951d04684d811938e0e37b5302dbd11
Justin <justin.warambourg@gmail.com>
2024-05-07 16:36:33 +0800

    launch spark cluster locally

---
+++ docker/spark/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SPARK_WORKLOAD=$1
+
+echo "SPARK_WORKLOAD: $SPARK_WORKLOAD"
+
+if [ "$SPARK_WORKLOAD" == "master" ];
+then
+    start-master.sh -p 7077
+elif [ "$SPARK_WORKLOAD" == "worker" ];
+then
+    start-worker.sh spark://spark-master:7077
+elif [ "$SPARK_WORKLOAD" == "history" ];
+then
+    start-history-server.sh
+elif [ "$SPARK_WORKLOAD" == "gateway" ];
+then
+    spark-submit --master spark://spark-master:7077 --deploy-mode client /opt/spark/spark-test.py /opt/spark/jars/hadoop-aws-3.3.4.jar /opt/spark/jars/aws-java-sdk-bundle-1.12.262.jar
+fi
