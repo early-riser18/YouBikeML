a0bc7b0643560229d4038452558bda22f7af801d
Justin <justin.warambourg@gmail.com>
2024-05-20 14:05:15 +0800

    add pyspark support to featurelib functions

--- etl/transform/features_lib.py
+++ etl/transform/features_lib.py
@@ -1,17 +1,31 @@
+import pyspark
 import pandas as pd
-import pyspark.sql
+import numpy as np
 from typing import Union
 from abc import ABC, abstractmethod
-from pyspark.sql.functions import month, dayofweek, hour
-from etl.extraction.youbike import extract_youbike_raw_data
-from etl.transform.clean_youbike_data import clean_youbike_data
+from pyspark.sql.functions import (
+    month,
+    dayofweek,
+    hour,
+    from_unixtime,
+    lead,
+    date_format,
+    isnan,
+    lag,
+    mean,
+)
 from utils.utils import (
     get_youbike_snapshot_data_for_time_range,
     get_weather_zone,
     get_latest_weather_data,
+    DB_Connection,
 )
-import numpy as np
+from pyspark.sql.window import Window
+from db.main import get_all_valid_stations_id
 from api import get_bike_station_status
+import os
+from etl.transform.spark_app import SparkApp
+from sqlalchemy import text
 
 
 class DataTransformer(ABC):
@@ -58,8 +72,28 @@ class MakeWeatherFeatures(DataTransformer):
 
         return df
 
-    def in_pyspark(self):
-        pass
+    def in_pyspark(self, df: pyspark.sql.DataFrame):
+        window_spec = Window.partitionBy("zone").orderBy("datetime")
+
+        df = df.withColumn(
+            "1h_fwd_apparent_temperature",
+            lead("apparent_temperature", 1).over(window_spec),
+        )
+        df = df.withColumn(
+            "1h_fwd_precipitation", lead("precipitation", 1).over(window_spec)
+        )
+
+        return df
+
+
+class StationOccupancyFeatures(DataTransformer):
+    def in_pandas(self, df):
+        df["pct_full"] = df["full"] / (df["full"] + df["empty"])
+        return df
+
+    def in_pyspark(self, df):
+        df = df.withColumn("pct_full", df.full / (df.full + df.empty))
+        return df
 
 
 class LagFeatures(DataTransformer):
@@ -81,8 +115,16 @@ class LagFeatures(DataTransformer):
 
         return df
 
-    def in_pyspark(self):
-        pass
+    def in_pyspark(self, df):
+        windowSpec = Window.partitionBy("station_id").orderBy("extraction_ts")
+
+        df = df.withColumn("30m_blag_pct_full", lag("pct_full", 3).over(windowSpec))
+
+        # Rolling average for 120 minutes (12 periods of 10 minutes)
+        df = df.withColumn(
+            "120m_avg_pct_full", mean("pct_full").over(windowSpec.rowsBetween(-11, 0))
+        )
+        return df
 
 
 class TimeFeatures(DataTransformer):
@@ -99,15 +141,6 @@ class TimeFeatures(DataTransformer):
             .withColumn("hour", hour("extraction_ts"))
         )
         return df
-
-
-class StationOccupancyFeatures(DataTransformer):
-    def in_pandas(self, df):
-        df["pct_full"] = df["full"] / (df["full"] + df["empty"])
-        return df
-
-    def in_pyspark(self):
-        pass
 
 
 class FormatToFeaturesSchema(DataTransformer):
@@ -220,6 +253,10 @@ class CreateInputPredictionFeatures(DataTransformer):
             on="weather_zone_id",
         )
 
+        # Some historical stations do no longer exist, based on whether they got a weather_zone_id assigned. If not dropped
+        ## Future work is to filter out all stations that do not exist anymore, or at least use them to learn by assigning them to a group
+        main_df = main_df.drop(index=main_df[main_df["weather_zone_id"].isna()].index)
+
         # Get weather data
         weather_data = get_latest_weather_data()
 
@@ -254,3 +291,120 @@ class CreateInputPredictionFeatures(DataTransformer):
 
     def in_pyspark(self):
         pass
+
+
+class CreateInputTrainingFeatures(DataTransformer):
+    def in_pandas(self):
+        """No implementation in pandas"""
+        pass
+
+    def in_pyspark(
+        self,
+        station_ids: list[int],
+        start_period: pd.Timestamp,
+        end_period: pd.Timestamp,
+    ):
+        """
+        1. Get historical data for time range
+        2. keep only requested stations ids
+        3. Get weather zone per station, merge weather zone id to name and merge historical weather data
+        4. Validate the features input schema
+        """
+        spark_app = SparkApp.get_instance()
+        spark_session = spark_app.spark_session
+        spark_context = spark_app.spark_context
+        URI = spark_context._gateway.jvm.java.net.URI
+        Path = spark_context._gateway.jvm.org.apache.hadoop.fs.Path
+        FileSystem = spark_context._gateway.jvm.org.apache.hadoop.fs.FileSystem
+
+        bucket_name = (
+            "stage-youbike" if os.environ["APP_ENV"] == "stage" else "local-youbike"
+        )
+        bucket_fs = FileSystem.get(
+            URI(f"s3a://{bucket_name}/"), spark_app.spark_hadoop_conf
+        )
+
+        # Import required data
+        hist_weather_reports_uri = [
+            obj.getPath().toString()
+            for obj in bucket_fs.listStatus(
+                Path(f"s3a://{bucket_name}/raw_data/weather/historical_report")
+            )
+        ]
+        hist_weather_report = (
+            spark_session.read.option("InferSchema", True)
+            .option("header", True)
+            .format("parquet")
+            .load(hist_weather_reports_uri)
+        )
+        hist_weather_report = hist_weather_report.dropDuplicates(["datetime", "zone"])
+
+        youbike_snapshot_uri = [
+            obj.getPath().toString()
+            for obj in bucket_fs.listStatus(Path(f"s3a://{bucket_name}/clean_data/"))
+        ][5000:5500]
+        hist_snapshot_df = (
+            spark_session.read.option("InferSchema", True)
+            .option("header", True)
+            .format("parquet")
+            .load(youbike_snapshot_uri)
+        )
+
+        with DB_Connection.from_env().connection as conn:
+            station_id_to_weather_id = spark_session.createDataFrame(
+                pd.read_sql('SELECT "id", "weather_zone_id"  FROM bike_station;', conn)
+            )
+            weather_zone_name_df = spark_session.createDataFrame(
+                pd.read_sql('SELECT "id", "name" FROM weather_zone;', conn)
+            )
+
+        # Only keep requested filters
+        hist_snapshot_df = hist_snapshot_df.filter(
+            hist_snapshot_df.id.isin(station_ids)
+        )
+
+        weather_zone_name_df = weather_zone_name_df.withColumnRenamed(
+            "id", "weather_zone_id"
+        ).withColumnRenamed("name", "weather_zone")
+
+        main_df = hist_snapshot_df.join(
+            station_id_to_weather_id, on="id", how="left"
+        ).join(weather_zone_name_df, on="weather_zone_id", how="left")
+
+        # Some historical stations do no longer exist, based on whether they got a weather_zone_id assigned. If not dropped
+        ## Future work is to filter out all stations that do not exist anymore, or at least use them to learn by assigning them to a group
+        main_df = main_df.filter(~isnan(main_df.weather_zone_id))
+
+        hist_weather_report = MakeWeatherFeatures("pyspark").run(hist_weather_report)
+        hist_weather_report = (
+            hist_weather_report.withColumn(
+                "datetime_unix", hist_weather_report.datetime / 10**9
+            )
+            .withColumn("datetime", from_unixtime("datetime_unix", "yyyy-MM-dd_HH"))
+            .withColumnRenamed("datetime", "y_m_d_h")
+            .withColumnRenamed("zone", "weather_zone")
+        )
+
+        main_df = main_df.withColumn(
+            "y_m_d_h", date_format(main_df.extraction_ts, "yyyy-MM-dd_HH")
+        ).withColumnRenamed("id", "station_id")
+
+        main_df = main_df.join(
+            hist_weather_report.select(
+                [
+                    "temperature",
+                    "relative_humidity",
+                    "apparent_temperature",
+                    "precipitation",
+                    "wind_speed",
+                    "y_m_d_h",
+                    "weather_zone",
+                    "1h_fwd_precipitation",
+                    "1h_fwd_apparent_temperature",
+                ]
+            ),
+            on=["y_m_d_h", "weather_zone"],
+            how="left",
+        )
+
+        return main_df
