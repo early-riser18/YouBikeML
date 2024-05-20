fa7800d80f02c021e0cb1953ea33151f44cab992
Justin <justin.warambourg@gmail.com>
2024-05-20 16:14:10 +0800

    update retrain_model script dir

--- predict/retrain_model.py
+++ predict/retrain_model.py
@@ -1,11 +1,11 @@
 import pandas as pd
-from db import main
+from db.main import get_all_valid_stations_id
 from etl.transform.spark_app import SparkApp
 from etl.transform import features_creator
 from etl.transform import features_creator
 
 spark_session = SparkApp.get_instance(log_level="DEBUG")
-test_station_ids = main.get_all_valid_stations_id()
+test_station_ids = get_all_valid_stations_id()
 
 training_features = features_creator.FeaturesCreator_v1().make_training_features(
     test_station_ids, pd.Timestamp("2024 04 15"), pd.Timestamp("2024 04 30")
